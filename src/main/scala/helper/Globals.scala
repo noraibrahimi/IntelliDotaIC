36ef46047331c6bb7a45d6785d68eef510f48825
Labinot Vila <vila_labinot@hotmail.com>
2019-10-15 10:50:25 +0200

    Naming

--- src/main/scala/helper/Globals.scala
+++ src/main/scala/helper/Globals.scala
@@ -13,7 +13,11 @@ object Globals {
 
 	// routes
 	val MAIN_ROUTE = "C:/Users/Labinot/Desktop/main_route/"
+
+	// classificatin routes
 	val FETCHED_STEAM_DATA = "fetched_steam_data"
 	val CLASSIFIED_MODEL = "classified_model"
+
+	// clustering routes
 	val KAGGLE_DATASET = "kaggle_dataset"
 }
