3f4c24ff99ee5e73e9039897c4c5d1b6a7a02a1b
Nora Ibrahimi <nibrahimi12@gmail.com>
2019-10-23 16:02:15 +0200

    Removing unused elements

--- src/main/scala/classification/Classification.scala
+++ src/main/scala/classification/Classification.scala
@@ -4,7 +4,6 @@ import helper.Constants
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.RandomForestClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{StandardScaler, VectorAssembler}
 import org.apache.spark.sql.SparkSession
 
